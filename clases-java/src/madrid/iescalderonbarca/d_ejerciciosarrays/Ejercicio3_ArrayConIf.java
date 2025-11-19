41b687ee44b66033f6ea7d0b4d6de7abd33ec0d0
Agustin Antonio Márquez Piña <agustin6041@outlook.es>
2025-11-19 17:25:38 +0100

    feat(ejercicios): agregar ejercicios de arrays unidimensionales y bidimensionales en Java para practicar manipulación de datos

---
+++ clases-java/src/madrid/iescalderonbarca/d_ejerciciosarrays/Ejercicio3_ArrayConIf.java
@@ -0,0 +1,32 @@
+package madrid.iescalderonbarca.d_ejerciciosarrays;
+
+/**
+ * Ejercicio 3: Array unidimensional con condicional if
+ * Crea un array de números enteros con 6 elementos y asígnales valores.
+ * Usa un bucle for para recorrer el array y un if para mostrar solo
+ * los números mayores que 5
+ * 
+ * int[] ejemplo = new int{4, 6, 5, 7, 2}
+ * 
+ * Eso significa que el Array
+ * tiene 5 cajitas, una cosa es el
+ * valor dentro de las cajitas y
+ * otra la posición que tienen
+ * las cajitas, ejemplo
+ * 
+ * Valores = [4][6][5][7][2]
+ * Posiciones = [0][1][2][3][4]
+ * 
+ * @author Agustín. A. Marquez. Piña
+ * @since 14/06/2025
+ * @see <a href="mailto:agu1406@outlook.es">agu1406@outlook.es</a>
+ * @see <a href="https://github.com/Agu1406/ClasesParticulares">Repositorio GitHub</a>
+ * @see <a href="https://agu1406.github.io/ClasesParticulares">GitHub Pages</a>
+ */
+public class Ejercicio3_ArrayConIf {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
