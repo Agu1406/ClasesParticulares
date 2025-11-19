c6d4bd7dd3c1ea676664542aee94a7bbb5b78588
Agustin Antonio Márquez Piña <agustin6041@outlook.es>
2025-11-19 17:26:06 +0100

    feat(ejercicios): agregar ejercicios de ArrayList en Java para practicar manipulación de listas y condicionales

---
+++ clases-java/src/madrid/iescalderonbarca/e_ejerciciosarraylist/Ejercicio9_ArrayListEliminarElementos.java
@@ -0,0 +1,24 @@
+package madrid.iescalderonbarca.e_ejerciciosarraylist;
+
+import java.util.ArrayList;
+
+/**
+ * Ejercicio 9: Eliminar elementos de un ArrayList con función
+ * Crea un ArrayList de números enteros y agrega 8 números.
+ * Crea una función llamada eliminarMenores que reciba el ArrayList y un número como parámetros,
+ * use un bucle while o for para eliminar todos los números menores que el número dado
+ * usando el método remove, y muestra el ArrayList antes y después de eliminar
+ * 
+ * @author Agustín. A. Marquez. Piña
+ * @since 14/06/2025
+ * @see <a href="mailto:agu1406@outlook.es">agu1406@outlook.es</a>
+ * @see <a href="https://github.com/Agu1406/ClasesParticulares">Repositorio GitHub</a>
+ * @see <a href="https://agu1406.github.io/ClasesParticulares">GitHub Pages</a>
+ */
+public class Ejercicio9_ArrayListEliminarElementos {
+    public static void main(String[] args) {
+        
+    }
+}
+
+
