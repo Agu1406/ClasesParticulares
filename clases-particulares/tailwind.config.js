291d2192f98bb26940310e515273e124c4a8d5c6
Agustin Antonio Márquez Piña <agustin6041@outlook.es>
2025-11-27 00:13:39 +0100

    feat(clases-particulares): update Tailwind CSS configuration and restructure index.css to use Tailwind directives. Added content paths for Tailwind and set base path for Vite project.

--- clases-particulares/tailwind.config.js
+++ clases-particulares/tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  /*
+  Incluimos todos los archivos de SRC de extensión JS/TS/JSX/TSX
+  y el index.html principal del proyecto.
+   */
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
--- clases-particulares/vite.config.ts
+++ clases-particulares/vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Base/ruta principal del proyecto con React.
+  base: "/clases-particulares",
 })
