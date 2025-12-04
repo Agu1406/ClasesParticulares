2734f73ddccd5a2931636a499a9d78165a06680e
Agustin Antonio Márquez Piña <agustin6041@outlook.es>
2025-12-04 02:00:16 +0100

    fix(vite.config): update base path configuration for production and development environments to ensure correct routing

--- clases-particulares/vite.config.ts
+++ clases-particulares/vite.config.ts
@@ -5,5 +5,4 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   // Base/ruta principal del proyecto con React.
-  base: "/clases-particulares",
-})
+  base: process.env.NODE_ENV === 'production' ? '/clases-particulares' : '/'})
