542a557236338fcc09116539df623d007297b8f7
Agustin Antonio Márquez Piña <agustin6041@outlook.es>
2025-12-04 20:17:35 +0100

    feat(languages): create languages data structure for programming languages and update Navigation component to include PHP link

---
+++ clases-particulares/src/data/languages.ts
@@ -0,0 +1,28 @@
+import type { Language } from "../types";
+/**
+ * Creación de los datos asociados a las interfaces diseñadas y creadas
+ * en el directorio de "types", puedes verlas en {@link Language} para
+ * entender mejor su estructura y comportamiento.
+ * 
+ * Aqui he de crear los datos de todos los lenguajes que espero poder
+ * compartir en mi sitio web de clases particulares, para ello usando
+ * como guía los directorios de las clases que ya he dado hasta el
+ * momento.
+ * 
+ * @author Agustín. A. Marquez. Piña
+ * @since 1.0.0
+ * 
+ * @see https://github.com/Agu1406/ClasesParticulares Repositorio GitHub
+ * @see https://agu1406.github.io/ClasesParticulares GitHub Pages
+ * */
+export const languages: Language[] = [
+    
+    {
+        id: "java",
+        name: "Java",
+        icon: " ",
+        description: "Programación orientada a objetos (POO), Swing, JavaFX, JDBC, Spring y más",
+        path: "/java"
+    },
+
+]
