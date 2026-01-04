49b5b8426dc3a515c55af52bbc1c2d6bdcbee554
Agu1406 <agustin6041@outlook.es>
2026-01-04 15:35:06 +0100

    feat(footer): enhance Footer component with social media links and improve layout for better responsiveness

--- portfolio/src/components/Footer.tsx
+++ portfolio/src/components/Footer.tsx
@@ -15,9 +15,14 @@ function Footer() {
     return (
         <footer className="bg-evergreen text-lavender p-4">
             <div className="container mx-auto text-center font-bold">
-                <div className="flex flex-col md:flex-row justify-between">
-                    <p>© 2025 - Portal de clases particulares de Agustín</p>
-                    <p>Hola</p>
+                 {/* Diseño pensado primero en movil y a partir de tamaños medianos ("md") en escritorio. */}
+                <div className="flex flex-col md:flex-row justify-between items-center">
+                    <p>© 2025 - Portal de clases particulares</p>
+                    <div className="flex space-x-6">
+                        <a href="https://github.com/Agu1406/ClasesParticulares" target="_blank">GitHub</a>
+                        <a href="https://agu1406.github.io/ClasesParticulares" target="_blank">GitHub Pages</a>
+                        <a href="https://www.linkedin.com/in/agustin6041/" target="_blank">LinkedIn</a>
+                    </div>
                 </div>
             </div>
         </footer>
