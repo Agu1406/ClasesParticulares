87033edae6a90d4b4034d134ec3829dcbaae0e00
Agu1406 <agustin6041@outlook.es>
2026-01-17 02:20:35 +0100

    Eliminar carpeta rn (React Native) del repositorio

--- rn/andalucia/sevilla/iesvelazquez/sevici/constants/colors.ts
+++
@@ -1,9 +0,0 @@
-export const Colors = {
-  primary: '#E60012',      // El rojo de Sevici
-  secondary: '#FFFFFF',    // Blanco
-  text: '#000000',         // Negro para textos
-  textSecondary: '#666666', // Gris para textos secundarios
-  error: '#E60012',       // El mismo rojo para errores
-  background: '#FFFFFF',   // Fondo blanco
-};
-
