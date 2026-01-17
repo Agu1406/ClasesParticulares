75dd97cd2740c8e90dce9aa95da6ab6c6938cd31
Agu1406 <agustin6041@outlook.es>
2026-01-17 02:18:04 +0100

    feat(exercises): add new Java exercises focusing on string manipulation, including functions to remove characters, hide vowels, remove accents, shorten strings, and alternate case in strings

---
+++ java/src/madrid/iescalderonbarca/ejercicios/Ejercicio17_OcultaVocales.java
@@ -0,0 +1,25 @@
+package madrid.iescalderonbarca.ejercicios;
+
+/**
+ * Ejercicio 17: Codifica la función ocultaVocales que recibe un String y lo devuelve cambiando
+ * todas las vocales sin acentuar presentes en él por caracteres 'X'.
+ * 
+ * @author Agustín. A. Marquez. Piña
+ * @since 14/01/2026
+ * @see <a href="mailto:agu1406@outlook.es">agu1406@outlook.es</a>
+ * @see <a href="https://github.com/Agu1406/ClasesParticulares">Repositorio GitHub</a>
+ * @see <a href="https://agu1406.github.io/ClasesParticulares">GitHub Pages</a>
+ */
+public class Ejercicio17_OcultaVocales {
+    public static void main(String[] args) {
+        // TODO: Implementa la función ocultaVocales y pruébala aquí
+        // Ejemplo de uso:
+        // String resultado = ocultaVocales("Hola mundo");
+        // System.out.println(resultado); // Debería imprimir "HXlX mXndX"
+    }
+    
+    // TODO: Implementa la función ocultaVocales aquí
+    // public static String ocultaVocales(String texto) {
+    //     // Tu código aquí
+    // }
+}
---
+++ java/src/madrid/iescalderonbarca/ejercicios/Ejercicio20_EscaleraCadena.java
@@ -0,0 +1,25 @@
+package madrid.iescalderonbarca.ejercicios;
+
+/**
+ * Ejercicio 20: Realiza la función escaleraCadena que recibe un String y devuelve otro en el que
+ * alterna los caracteres del parámetro entre mayúsculas y minúsculas.
+ * 
+ * @author Agustín. A. Marquez. Piña
+ * @since 14/01/2026
+ * @see <a href="mailto:agu1406@outlook.es">agu1406@outlook.es</a>
+ * @see <a href="https://github.com/Agu1406/ClasesParticulares">Repositorio GitHub</a>
+ * @see <a href="https://agu1406.github.io/ClasesParticulares">GitHub Pages</a>
+ */
+public class Ejercicio20_EscaleraCadena {
+    public static void main(String[] args) {
+        // TODO: Implementa la función escaleraCadena y pruébala aquí
+        // Ejemplo de uso:
+        // String resultado = escaleraCadena("Hola mundo");
+        // System.out.println(resultado); // Debería imprimir "HoLa MuNdO"
+    }
+    
+    // TODO: Implementa la función escaleraCadena aquí
+    // public static String escaleraCadena(String texto) {
+    //     // Tu código aquí
+    // }
+}
