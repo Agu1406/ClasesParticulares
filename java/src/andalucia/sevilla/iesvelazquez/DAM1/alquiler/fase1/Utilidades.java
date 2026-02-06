3a8b7f8f4bedbe925fe09ab0e34afbedc60581a5
Agu1406 <agustin6041@outlook.es>
2026-02-06 10:46:00 +0100

    feat(Practica AlquileresCoches): add initial PDF document outlining the requirements and structure for a car rental management application whit different versions of the same application making possible to see how it goes from zero to a full resolved excersice.

---
+++ java/src/andalucia/sevilla/iesvelazquez/DAM1/alquiler/fase1/Utilidades.java
@@ -0,0 +1,16 @@
+package andalucia.sevilla.iesvelazquez.DAM1.alquiler.fase1;
+
+public class Utilidades {
+    public static boolean validarMatricula(String matricula) {
+        return matricula != null && matricula.matches("^[0-9]{4}[A-Z]{3}$");
+    }
+
+    public static boolean validarCodigoPostal(String cp) {
+        return cp != null && cp.matches("^[0-9]{5}$");
+    }
+
+    public static boolean validarDni(String dni) {
+        return dni != null && dni.matches("^[0-9]{8}[A-Z]$");
+    }
+}
+
---
+++ java/src/andalucia/sevilla/iesvelazquez/DAM1/alquiler/fase2/v3/Furgoneta.java
@@ -0,0 +1,43 @@
+package andalucia.sevilla.iesvelazquez.DAM1.alquiler.fase2.v3;
+
+public final class Furgoneta extends Mercancias {
+    private boolean refrigerado;
+    private Tamano tamano;
+
+    public Furgoneta(boolean refrigerado, int pma, int volumen, String matricula, String marca, String modelo, int cilindrada) {
+        super(pma, volumen, matricula, marca, modelo, cilindrada);
+        this.refrigerado = refrigerado;
+        if (volumen <= 10) {
+            this.tamano = Tamano.PEQUENA;
+        } else if (volumen <= 20) {
+            this.tamano = Tamano.MEDIANA;
+        } else {
+            this.tamano = Tamano.GRANDE;
+        }
+    }
+
+    public boolean getRefrigerado() {
+        return refrigerado;
+    }
+
+    public Tamano getTamano() {
+        return tamano;
+    }
+
+    @Override
+    public String toString() {
+        return "Furgoneta{" +
+                "matricula='" + getMatricula() + '\'' +
+                ", marca='" + getMarca() + '\'' +
+                ", modelo='" + getModelo() + '\'' +
+                ", cilindrada=" + getCilindrada() +
+                ", disponible=" + isDisponible() +
+                ", baja=" + isBaja() +
+                ", pma=" + getPMA() +
+                ", volumen=" + getVolumen() +
+                ", refrigerado=" + refrigerado +
+                ", tamano=" + tamano +
+                '}';
+    }
+}
+
