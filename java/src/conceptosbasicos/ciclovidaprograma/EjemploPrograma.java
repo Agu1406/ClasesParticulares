758eb2798645bb71a62e0186062a72b737023931
Agu1406 <agustin6041@outlook.es>
2026-02-15 17:57:08 +0100

    feat(documentation): add README.md and example Java programs to introduce basic concepts and syntax in Java programming.

---
+++ java/src/conceptosbasicos/ciclovidaprograma/EjemploPrograma.java
@@ -0,0 +1,20 @@
+// La linea llamada "package" podeís ignorarla por completo, mucho más adelante se explica.
+package conceptosbasicos.ciclovidaprograma;
+
+/**
+ * En el README hablo sobre esto, el nombre de nuestro archivo es
+ * EjemploPrograma.java y cuando escribimos el código tenemos que
+ * decirle a Java el nombre del archivo, por eso aquí vemos el
+ * mismo nombre sin la extensión (.java).
+ * 
+ * Este programa esta vacio, por lo tanto, no tiene errores, cuando
+ * lo ejecutamos con el compilador de java (javac) este si no detecta
+ * ningún problema lo tranforma en un archivo del tipo ".class" el 
+ * cual podemos ejecutar en cualquier ordenador que tenga isntalado
+ * Java.
+ */
+public class EjemploPrograma {
+    public static void main(String[] args) {
+
+    }
+}
