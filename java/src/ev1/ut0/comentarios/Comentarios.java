5a65eefc56cd8b93f46e8ddb8d966c64c66ac34c
Agu1406 <agustin6041@outlook.es>
2026-02-20 14:55:32 +0100

    feat(documentation): enhance README.md with new path for Comentarios.java and update examples for single-line and multi-line comments in Java

--- java/src/ev1/ut0/comentarios/Comentarios.java
+++ java/src/ev1/ut0/comentarios/Comentarios.java
@@ -12,7 +12,22 @@ package ev1.ut0.comentarios;
 public class Comentarios {
     public static void main(String[] args) {
         /*
-        
+        El otro tipo de comentario que existe es el comentario multilinea
+        tradicional, empieza con barra inclinada "/" seguida de un asterico.
+
+        Y termina con asterico seguido de una barra inclinada "/" sienfo este
+        comentario el ejemplo vivo de ello, entre el "inicio" y el "final" de
+        este comentario puedo escribir todas las lineas de información que
+        quiero con menos peso profesional y documentativo que el comentario
+        explicando anteriormente a este ,el "JavaDoc".
         */
+
+        // Finalmente existen los comentarios de una sola linea.
+
+        // Se escriben con doble barra inclinada "//" y ocupan una sola linea.
+
+        // Se usan para explicaciones o notas muy breves y no pueden ocupar más de una linea.
+
+        // Solo tienen inicio, el final es el propio final de la linea como tal.
     }
 }
