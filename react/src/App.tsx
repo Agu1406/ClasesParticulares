aa7eebd54ccc9514da78e09ade70b33387a98481
Agu1406 <agustin6041@outlook.es>
2026-03-15 14:24:00 +0100

    feat(App): integrate MyApp component into main application; add comment for clarity on React tutorial usage.

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+// Importo la "app" principal del tuturial de REACT que estoy siguiendo.
+import MyApp from './components/MyApp'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <MyApp />
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
