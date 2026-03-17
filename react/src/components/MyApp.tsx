e659c5ea376995074bf057f6c6158b87e1921e8b
Agu1406 <agustin6041@outlook.es>
2026-03-17 23:23:17 +0100

    feat(MyApp, MyButton): enhance MyApp and MyButton components for React learning; add user data display and improve styling for better presentation.

--- react/src/components/MyApp.tsx
+++ react/src/components/MyApp.tsx
@@ -10,10 +10,41 @@
  */
 import MyButton from './MyButton';
 
+/**
+ * React puede leer datos de diversas fuentes, entre ellas, por ejemplo, JSON
+ * o directamente desde una API o backend, ahora aprenderemos a mostrar datos
+ * en los componentes provinientes de otras fuentes, vamos a simular un JSON
+ * con datos ficticios de un usuario.
+ */
+const user = {
+    name: "Agustín Antonio Márquez Piña",
+    imgURL: "https://avatars.githubusercontent.com/u/158044061?v=4",
+    imgSize: "90" 
+};
+
 export default function MyAPP() {
     return (
         <>
-            <h1>Mi primera aplicación</h1>
+            <h1>¡Aprendiendo React desde cero!</h1>
+            <br />
+            {/**
+             * Inserto mi nombre desde la constante con los datos del usuario que
+             * he creado previamente.
+             */}
+            <h2>Mi nombre es {user.name}</h2>
+            {/**
+             * De la misma forma todos los datos de la imagen los puedo obtener de
+             * la constante "user", la ruta de la imagen (src), el ancho y alto de
+             * la imagén e incluso general el "alt" combinando un String con el
+             * nombre de usuario.
+             */}
+            <img 
+                src={user.imgURL} 
+                alt={"Imagen de perfil de " + user.name} 
+                width={user.imgSize}
+                height={user.imgSize}
+                />
+            <br />
             {/**
              * El componente "MyButton" se importa desde un archivo TSX diferente y
              * se renderiza aquí, es como usar "legos" para construir nuestras
