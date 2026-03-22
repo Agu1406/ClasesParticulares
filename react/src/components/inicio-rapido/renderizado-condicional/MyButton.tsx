6e33ba50cff63a71bb7423cb970633cfdc10f86f
Agu1406 <agustin6041@outlook.es>
2026-03-22 21:45:32 +0100

    feat(AdminPanel, LoginForm, styles): introduce AdminPanel and LoginForm components for user management; add CSS for layout styling to enhance user interface.

---
+++ react/src/components/inicio-rapido/renderizado-condicional/MyButton.tsx
@@ -0,0 +1,13 @@
+/**
+ * Aprendizaje de REACT desde cero siguiendo los ejemplos y tutoriales encontrados
+ * en el sitio web oficial de REACT.
+ * @see https://es.react.dev/learn 
+ * @returns Botón de ejemplo.
+ */
+import './styles.css';
+
+export default function MyButton() {
+    return (
+        <button className="boton-tutorial">Soy un botón</button>
+    );
+}
