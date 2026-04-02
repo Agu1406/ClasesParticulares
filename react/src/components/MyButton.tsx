c596334245578e74f7481c15db819ca69670a93d
Agu1406 <agustin6041@outlook.es>
2026-04-02 16:50:35 +0200

    feat(MyButton): integrate useState for click handling; update button functionality to display current count on each click.

--- react/src/components/MyButton.tsx
+++ react/src/components/MyButton.tsx
@@ -6,21 +6,54 @@
  */
 import './styles.css';
 
+/**
+ * ACTUALIZACIÓN DE PAGINAS.
+ * 
+ * Usaremos el "hook" de react "useSate" para alterar el estado de
+ * un componente y/o variable.
+ */
+import { useState } from 'react';
+
 export default function MyButton() {
     /**
+     * La sintaxis es:
+     * 
+     * - Variable cuyo estado o valor puede modificarse ("count").
+     * - Función encargada de modificar dicho valor / estado ("setCount").
+     * - Función "useState" con el valor inicial de "count" siendo "0".
+     * 
+     * useState sabe el valor inicial de "count" (es "0") y es la encargada
+     * de "guardar" el valor actual de la variable.  
+     */
+    const [count, setCount] = useState(0);
+
+    /**
+     * RESPONDER A EVENTOS.
+     * 
      * Esta es una función interna del componente que permite
      * manejar el comportamiento de este botón en especifico
      * si hacen click en el.
+     * 
+     * ACTUALIZAR PAGINA.
+     * 
+     * Ya tenemos una función que "maneja" el click del botón, solo vamos
+     * a incorporar el useState para que incremente el valor de count en
+     * cada click y lo muestr en el componente.
      */
-    function handleClick () {
+    function handleClick() {
+        // RESPONDER A EVENTO.
         alert("¡Haz hecho click en el botón");
+        
+        // ACTUALIZAR PAGINA.
+        setCount(count + 1);
     }
 
     return (
         <div className="learn-react">
             <button type="button" onClick={handleClick}>
                 Soy un botón
-                </button>
+            </button>
+            <p>Valor actual {count}</p>
         </div>
     );
 }
