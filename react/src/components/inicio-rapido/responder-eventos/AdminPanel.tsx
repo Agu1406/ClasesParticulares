23ed4e99f8057d201095ea8b87cbdf63e0732083
Agu1406 <agustin6041@outlook.es>
2026-04-02 02:28:14 +0200

    feat(MyButton, AdminPanel, LoginForm, MyApp, styles): implement new components for user interaction and management; enhance MyButton with click handling and improve styling for better user experience.

---
+++ react/src/components/inicio-rapido/responder-eventos/AdminPanel.tsx
@@ -0,0 +1,25 @@
+import './styles.css';
+
+const user = {
+    name: "Agustín Antonio Márquez Piña",
+    imgURL: "https://avatars.githubusercontent.com/u/158044061?v=4",
+    imgSize: "90"
+};
+
+export default function AdminPanel() {
+    return (
+        <>
+            <div className="learn-react">
+                <h2>Panel ficticio de administración de usuario</h2>
+                <br />
+                <img
+                    src={user.imgURL}
+                    alt={'Imagén de perfil de: ' + user.name}
+                    width={user.imgSize}
+                    height={user.imgSize}
+                />
+                <h2>Usuario: {user.name}</h2>
+            </div>
+        </>
+    );
+}
