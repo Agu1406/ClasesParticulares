140d08dd2547d7b8bab2d061589779c28c686cd1
Agu1406 <agustin6041@outlook.es>
2026-04-02 02:17:31 +0200

    feat(AdminPanel, LoginForm, MyButton, MyApp, styles): introduce new components for user management and interaction; enhance styling and layout for improved user experience.

---
+++ react/src/components/inicio-rapido/renderizado-listas/LoginForm.tsx
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @returns Formulario de inicio de sesión ficticio para aprender react.
+ */
+import './styles.css';
+
+export default function LoginForm() {
+    return (
+        <>
+            <div className="learn-react">
+                <form action="">
+                    <label htmlFor="user">Usuario: </label>
+                    <input type="text" id="user" name="user" />
+                    <br />
+                    <label htmlFor="password">Contraseña: </label>
+                    <input type="text" name="password" id="password" />
+                    <br />
+                    <button type="submit">¡Iniciar sesión!</button>
+                </form>
+            </div>
+        </>
+    );
+}
---
+++ react/src/components/inicio-rapido/renderizado-listas/MyButton.tsx
@@ -0,0 +1,15 @@
+/**
+ * Aprendizaje de REACT desde cero siguiendo los ejemplos y tutoriales encontrados
+ * en el sitio web oficial de REACT.
+ * @see https://es.react.dev/learn 
+ * @returns Botón de ejemplo.
+ */
+import './styles.css';
+
+export default function MyButton() {
+    return (
+        <div className="learn-react">
+            <button type="button">Soy un botón</button>
+        </div>
+    );
+}
